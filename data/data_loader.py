e6b560bc453255f37f5269f74c5ceb7a1779f96f
ileumbbb <1208495124@qq.com>
2024-06-26 21:41:40 +0800

    Add files via upload

---
+++ data/data_loader.py
@@ -0,0 +1,28 @@
+import data.cifar10 as cifar10
+
+
+def load_data(dataset, root, num_query, num_train, batch_size, num_workers):
+    """
+    Load dataset.
+
+    Args
+        dataset(str): Dataset name.
+        root(str): Path of dataset.
+        num_query(int): Number of query data points.
+        num_train(int): Number of training data points.
+        num_workers(int): Number of loading data threads.
+
+    Returns
+        query_dataloader, train_dataloader, retrieval_dataloader(torch.evaluate.data.DataLoader): Data loader.
+    """
+    if dataset == 'cifar-10':
+        query_dataloader, train_dataloader, retrieval_dataloader = cifar10.load_data(root,
+                                                                                     num_query,
+                                                                                     num_train,
+                                                                                     batch_size,
+                                                                                     num_workers,
+                                                                                     )
+    else:
+        raise ValueError("Invalid dataset name!")
+
+    return query_dataloader, train_dataloader, retrieval_dataloader
